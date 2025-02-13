6570ae64f60e43ca8a287eb6ae39b8c5bccb1c07
TheWhiteCreator <cdiasdealmeida81@hotmail.com>
2025-02-13 18:27:09 +0100

    1.0

--- versions/1.0/CODE/__init__.py
+++
@@ -1,6 +0,0 @@
-# pywat/__init__.py
-from .file_tools import * 
-from .system_tools import *  
-from .registry_tools import *  
-from .cmd_tools import *  
-from .utils import *  
--- versions/1.0/CODE/utils.py
+++
@@ -1,7 +0,0 @@
-# pywat/utils.py
-
-def log_message(message):
-    """
-    Logs a message with a standard PyWAT prefix.
-    """
-    print(f"[PyWAT LOG] {message}")
